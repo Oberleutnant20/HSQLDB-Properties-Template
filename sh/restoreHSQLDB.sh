92923721440490af25eb1540927d99383e26100e
Tim <tim.lorse@gmx.de>
2018-04-06 12:50:30 +0200

    Update restoreHSQLDB.sh

--- sh/restoreHSQLDB.sh
+++ sh/restoreHSQLDB.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-cd <Speicherort/von/hsqldb/lib/>
-java -cp hsqldb.jar org.hsqldb.lib.tar.DbBackupMain -extract ../.backups/<datenbank>.tar <SpeicherortDB>
+cd <Speicherort/von/hsqldb>/lib/
+read -p "Geben Sie den Namen des Backup ein, welches Sie wieder herstellen wollen: " name
+read -p "Geben Sie den Pfad zum Speicherort der Datenbank an: " path
+java -cp hsqldb.jar org.hsqldb.lib.tar.DbBackupMain -extract ../.backups/$name.tar $path
