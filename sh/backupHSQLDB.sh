ea458f32510588ba457473df3ad9443aea071dcb
Tim <tim.lorse@gmx.de>
2018-04-06 12:51:12 +0200

    Update backupHSQLDB.sh

--- sh/backupHSQLDB.sh
+++ sh/backupHSQLDB.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cd <path/to/hsqldb>/lib/
-read -p "Gib Sie einen Namen für das Backup ein: " name
-java -cp <hsqldb-version>.jar org.hsqldb.lib.tar.DbBackupMain --save ../.backups/$name.tar <Speicherort_der Datenbank>
+read -p "Geben Sie einen Namen für das Backup ein: " name
+read -p "Geben Sie den Pfad zum Speicherort der Datenbank an: " path
+java -cp <hsqldb-version>.jar org.hsqldb.lib.tar.DbBackupMain --save ../.backups/$name.tar $path
