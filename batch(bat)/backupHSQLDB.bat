a04bffd0b99c875e4b12abfcd21503d983f3bdff
Tim <tim.lorse@gmx.de>
2018-04-06 12:52:26 +0200

    Update backupHSQLDB.bat

--- batch(bat)/backupHSQLDB.bat
+++ batch(bat)/backupHSQLDB.bat
@@ -1,4 +1,5 @@
 @echo off
 cd <Path/to/HSQLDB>/lib/
 set /p name=Geben Sie einen Namen für das Backup an: 
-java -cp hsqldb.jar org.hsqldb.lib.tar.DbBackupMain --save ../.backup/%name%.tar <speicherort>
+set /p path=Geben Sie den Pfad zum Speicherort der Datenbank an: 
+java -cp hsqldb.jar org.hsqldb.lib.tar.DbBackupMain --save ../.backup/%name%.tar %path%
