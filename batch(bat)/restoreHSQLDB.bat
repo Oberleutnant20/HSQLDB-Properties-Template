c19cf6094a014d5429d1e428789e2cde5276a63a
Tim Lorse <tim.lorse@gmx.de>
2018-04-19 17:39:39 +0200

    Update restoreHSQLDB.bat

--- batch(bat)/restoreHSQLDB.bat
+++ batch(bat)/restoreHSQLDB.bat
@@ -2,4 +2,4 @@
 cd <path/to/hsqldb>/lib/
 set /p name=Geben Sie den Namen des Backups an: 
 set /p path=Geben Sie den Pfad zum Speicherort der Datenbank an: 
-java -cp hsqldb.jar org.hsqldb.lib.tar.DbBackupMain --extract ../.backup/%name%.tar %path%
+java -cp hsqldb.jar org.hsqldb.lib.tar.DbBackupMain --extract ../_backup/%name%.tar %path%
